ff7018cb45d76f934f9b2cb9e6ef1870ce455dbf
4b <4b@INF10>
2022-04-23 11:49:29 +0200

    versione 1

---
+++ src/Principale.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author rocco
+ * 
+ * @version 1.0
+ *
+ */
+public class Principale {
+
+	public static void main(String[] args) {
+		
+		System.out.println("prova gitHub 1");
+
+	}
+
+}
